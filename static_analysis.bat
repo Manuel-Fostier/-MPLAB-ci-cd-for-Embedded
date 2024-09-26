58a208086441803c88b7fa29c78c63979ed9d55b
Manuel Fostier <manuel.fostier@gmail.com>
2024-09-26 08:47:17 +0200

    restauration d'un prj cppcheck qui sera plus userfriendly. Il n'est pas encore au point.

--- static_analysis.bat
+++ static_analysis.bat
@@ -1,17 +1,8 @@
 @echo off
-REM Define the src folder
-SET src_folder=.\src
-
-REM Define the include directories for cppcheck
-SET CPPCHECK_INCLUDES= -I %src_folder%\config\default 
 
 REM Define the cppcheck flags
 SET CPPCHECK_FLAGS=--quiet --enable=all --error-exitcode=1 ^
-    --inline-suppr ^
-    --suppress=missingIncludeSystem ^
-    --suppress=unmatchedSuppression ^
-    --suppress=unusedFunction ^
-	--suppress=*:%src_folder%\config\*
+    --inline-suppr
 
 REM Running cppcheck
-cppcheck %CPPCHECK_FLAGS% %CPPCHECK_INCLUDES% %CPPCHECK_IGNORE% %src_folder%
+cppcheck %CPPCHECK_FLAGS% --project=./.cppcheck
